464caed143273362b0741a38afa45ed87b82422d
nikitacloudwapp <nikita.cloudwapp@gmail.com>
2022-11-04 18:59:20 +0530

    Playnxt04-11-22_18:59

--- app/src/main/java/com/cw/playnxt/server/Allurls.java
+++ app/src/main/java/com/cw/playnxt/server/Allurls.java
@@ -4,9 +4,9 @@ public class Allurls {
   /*  public static final String MainURL =" https://cloudwapp.in/RJ/playnext/";
     public static final String IMAGEURL ="https://cloudwapp.in/RJ/playnext"; */
 
-    public static final String MainURL = "https://cloudwapp.in/RJ/playnxt/playnext/";
-    public static final String IMAGEURL ="https://cloudwapp.in/RJ/playnxt/playnext";
-    public static final String WEBVIEWURL ="https://cloudwapp.in/RJ/playnxt/webview/";
+    public static final String MainURL = "https://playnxt.app/playnext/";
+    public static final String IMAGEURL ="https://playnxt.app/playnext";
+    public static final String WEBVIEWURL ="https://playnxt.app/webview/";
     public static final String share_app_url ="https://www.google.com/";
 
 }
